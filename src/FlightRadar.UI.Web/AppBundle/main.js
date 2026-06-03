f13f3b3ece29ad501f2f6332cd7815b79e2ebf50
Sebastian Haba <sebastian@haba.net.pl>
2026-06-03 21:41:23 +0200

    Poprawka do hostingu na dockerze

--- src/FlightRadar.UI.Web/AppBundle/main.js
+++ src/FlightRadar.UI.Web/AppBundle/main.js
@@ -1,12 +1,12 @@
 ﻿import { dotnet } from './_framework/dotnet.js'
 
+const url = new URL(globalThis.location.href);
+url.searchParams.set('hub', `${globalThis.location.origin}/hubs/radar`);
+globalThis.history.replaceState(null, '', url);
+
 const dotnetRuntime = await dotnet
     .withDiagnosticTracing(false)
     .withApplicationArgumentsFromQuery()
-    .create({
-        environmentVariables: {
-            HUB_URL: `${globalThis.location.origin}/hubs/radar`
-        }
-    });
+    .create();
 
 await dotnetRuntime.runMain();
--- src/FlightRadar.UI.Web/Program.cs
+++ src/FlightRadar.UI.Web/Program.cs
@@ -6,9 +6,19 @@ using Avalonia.Browser;
 
 internal partial class Program
 {
-    private static async Task Main(string[] args) => await BuildAvaloniaApp()
+    private static async Task Main(string[] args)
+    {
+        var hubArg = args.FirstOrDefault(a => a.StartsWith("--hub="));
+        if (hubArg != null)
+        {
+            var hubUrl = hubArg["--hub=".Length..];
+            Environment.SetEnvironmentVariable("HUB_URL", hubUrl);
+        }
+
+        await BuildAvaloniaApp()
             .WithInterFont()
             .StartBrowserAppAsync("out");
+    }
 
     public static AppBuilder BuildAvaloniaApp()
         => AppBuilder.Configure<FlightRadar.UI.App>();
