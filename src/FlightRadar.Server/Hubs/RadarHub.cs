5f2a19580318dd6e7b957172da128a5921560c50
Sebastian Haba <sebastian@haba.net.pl>
2026-06-05 14:30:08 +0200

    Po polaczeniu pobranie ostatniego stanu, bez czekania. Ujednolicenie

--- src/FlightRadar.Server/Hubs/RadarHub.cs
+++ src/FlightRadar.Server/Hubs/RadarHub.cs
@@ -1,11 +1,23 @@
+using FlightRadar.Server.Services;
 using Microsoft.AspNetCore.SignalR;
 
 namespace FlightRadar.Server.Hubs;
 
 public class RadarHub : Hub
 {
+    private readonly AdsbPoller _poller;
+
+    public RadarHub(AdsbPoller poller)
+    {
+        _poller = poller;
+    }
+
     public override async Task OnConnectedAsync()
     {
+        var state = _poller.LatestState;
+        if (state is not null)
+            await Clients.Caller.SendAsync("RadarUpdate", state);
+
         await base.OnConnectedAsync();
     }
 }
